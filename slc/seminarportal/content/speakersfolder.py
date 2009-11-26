be760307cf11cf0746070791b8567c1dde19195f
JC Brand <jcbrand>
2009-11-26 15:59:20 +0000

    Cleanup

--- slc/seminarportal/content/speakersfolder.py
+++ slc/seminarportal/content/speakersfolder.py
@@ -1,24 +1,16 @@
 """Definition of the SPSpeakersFolder content type
 """
 
-from zope.interface import implements, directlyProvides
+from zope.interface import implements
 
 from Products.Archetypes import atapi
 from Products.ATContentTypes.content import folder
 from Products.ATContentTypes.content import schemata
 
-from slc.seminarportal import seminarportalMessageFactory as _
 from slc.seminarportal.interfaces import ISpeakersFolder
 from slc.seminarportal.config import PROJECTNAME
 
-SPSpeakersFolderSchema = folder.ATFolderSchema.copy() + atapi.Schema((
-
-    # -*- Your Archetypes field definitions here ... -*-
-
-))
-
-# Set storage on fields copied from ATFolderSchema, making sure
-# they work well with the python bridge properties.
+SPSpeakersFolderSchema = folder.ATFolderSchema.copy() 
 
 SPSpeakersFolderSchema['title'].storage = atapi.AnnotationStorage()
 SPSpeakersFolderSchema['description'].storage = atapi.AnnotationStorage()
