7dc939e0dd0caa4d7b2c7aa3dcb1d31831cdf1c8
JC Brand <jcbrand>
2011-01-12 18:13:55 +0000

    Made the labels and descriptions 18n aware

--- src/slc/seminarportal/portlets/interfaces.py
+++ src/slc/seminarportal/portlets/interfaces.py
@@ -9,39 +9,39 @@ class ISpeakerPortlet(IPortletDataProvider):
     """Interface for portlet to display seminar speakers
     """
     featured_speakers = schema.List(
-                    title=u"Choose a speaker",
+                    title=_(u"Choose a speaker"),
                     description= \
-                            u'This field is not required if you choose to '
+                            _(u'This field is not required if you choose to '
                             u'randomly display speakers or show only speakers '
-                            u'from the current seminar.',
+                            u'from the current seminar.'),
                     required=False,
                     value_type=schema.Choice(
                         vocabulary="slc.seminarportal.vocabularies.speakers")
                     )
                         
     count = schema.Int( 
-                    title=u"How many speakers should be displayed?",
+                    title=_(u"How many speakers should be displayed?"),
                     description= \
-                            u"If you choose more speakers than should be "
+                            _(u"If you choose more speakers than should be "
                             u"displayed, the shown speakers will be chosen "
-                            u"randomly from your specified list.",
+                            u"randomly from your specified list."),
                     required=True,
                     default=5
                     )
                          
     random = schema.Bool(
-                    title=u'Option: Click here to display random speakers',
+                    title=_(u'Option: Click here to display random speakers'),
                     required=False,
                     default=True,
                     )
 
     local = schema.Bool(
-                    title=u'Option: Click here to display only speakers '
-                        u'from the current Seminar.',
-                    description=u'This option will only take effect if the '
+                    title=_(u'Option: Click here to display only speakers '
+                        u'from the current Seminar.'),
+                    description=_(u'This option will only take effect if the '
                         u'portlet is displayed inside a Seminar. Selecting '
                         u'this option takes precedence over the "random" '
-                        u'selection above.',
+                        u'selection above.'),
                     required=False,
                     default=False,
                     )
@@ -53,7 +53,7 @@ class ISpeechesPortlet(IPortletDataProvider):
     """Interface for portlet to display seminar speeches 
     """
     count = schema.Int( 
-                    title=u'How many speeches should be displayed?',
+                    title=_(u'How many speeches should be displayed?'),
                     required=True,
                     default=5
                     )
