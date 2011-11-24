81c0e924602867f71551133e5d25cb6aa6479a94
Cillian <cillianderoiste>
2011-11-24 10:05:54 +0000

    Embedding the required JavaScript for the speaker portlet overlay directly into the portlet template

---
+++ src/slc/seminarportal/browser/resources/portlets_speaker.js
@@ -0,0 +1,30 @@
+var SEMINAR = {}
+
+SEMINAR.loadSpeakerDetails = function () {
+    jQuery.fancybox.showActivity();
+    jQuery.ajax({
+        async: false,
+        type: 'GET',
+        url: this.href,
+        success: function(data) {
+            jQuery("#speaker-overlay #content")
+                .replaceWith(jQuery(data)
+                             .find("#content"));
+        }
+    });
+};
+
+jQuery(document).ready(function() {
+    if (jQuery("a.speaker-fancybox").length>0) {
+        jQuery("a.speaker-fancybox").fancybox({
+            'transitionIn'   : 'elastic',
+            'transitionOut'  : 'elastic',
+            'titlePosition'  : 'over',
+            'overlayOpacity' : 0.7,
+            'overlayColor'   : '#000',
+            'showNavArrows'  : false,
+            'onStart'        : SEMINAR.loadSpeakerDetails,
+            'content'        : jQuery('#speaker-overlay'),
+        });
+    }
+})
--- src/slc/seminarportal/browser/resources/seminar_view.js
+++ src/slc/seminarportal/browser/resources/seminar_view.js
@@ -4,34 +4,3 @@ jq(document).ready(function(){
         clickZone : 'div:eq(0)' // jQuery parent selector
     });
 });
-
-var SEMINAR = {}
-
-SEMINAR.loadSpeakerDetails = function () {
-    jQuery.fancybox.showActivity();
-    jQuery.ajax({
-        async: false,
-        type: 'GET',
-        url: this.href,
-        success: function(data) {
-            jQuery("#speaker-overlay #content")
-                .replaceWith(jQuery(data)
-                             .find("#content"));
-        }
-    });
-};
-
-jQuery(document).ready(function() {
-    if (jQuery("a.speaker-fancybox").length>0) {
-        jQuery("a.speaker-fancybox").fancybox({
-            'transitionIn'   : 'elastic',
-            'transitionOut'  : 'elastic',
-            'titlePosition'  : 'over',
-            'overlayOpacity' : 0.7,
-            'overlayColor'   : '#FFF',
-            'showNavArrows'  : false,
-            'onStart'        : SEMINAR.loadSpeakerDetails,
-            'content'        : jQuery('#speaker-overlay'),
-        });
-    }
-})
