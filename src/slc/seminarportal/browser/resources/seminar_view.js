b5df5df4fd1c584ad4ae94389f918939ac3b39cd
Jure Cerjak <jcerjak>
2013-05-31 16:36:32 +0000

    don't use jQuery.noConflict, seems to be causing conflicts with other scripts

--- src/slc/seminarportal/browser/resources/seminar_view.js
+++ src/slc/seminarportal/browser/resources/seminar_view.js
@@ -1,9 +1,7 @@
-var jq = jQuery.noConflict();
-
-jq(document).ready(function(){
+jQuery(document).ready(function(){
     "use strict";
 
-    jq("div.big-target a").bigTarget({
+    jQuery("div.big-target a").bigTarget({
         clickZone : 'div:eq(0)' // jQuery parent selector
     });
 });
